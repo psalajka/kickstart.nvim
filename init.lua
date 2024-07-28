122c25cd75f548354076132428362d7a9b8b138f
Petr Salajka <petr.salajka@gmail.com>
2024-07-28 11:15:30 +0200

    Add `fugitive` custom plugin

--- init.lua
+++ init.lua
@@ -892,6 +892,7 @@ require('lazy').setup({
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
   require 'custom.plugins.copilot',
+  require 'custom.plugins.fugitive',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
